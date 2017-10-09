539095f532460b1ba6cc7fe4f9478402a173628f
Ariel Espinoza <a.espinozasalinas@gmail.com>
2017-10-09 13:11:06 -0300

    responsive+++

--- index.php
+++ index.php
@@ -287,50 +287,9 @@
     <!-- scripts-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script src="js/custom.js"></script>
     <script type="text/javascript" src="js/jquery.jigowatt.js"></script><!-- AJAX Form Submit -->
     <script type="text/javascript" src="js/wow.min.js"></script>
-    <script>
-      //wow
-      var ancho = $(window).width();
-        if(ancho > 900){
-          wow = new WOW(
-            {
-              animateClass: 'animated',
-              offset:       100,
-              callback:     function(box) {
-                console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
-              }
-            }
-          );
-          wow.init();
-        }
-      //precarga imágenes
-      $(document).ready(function(){
-        var ancho = $(window).width();
-        if(ancho < 1440){
-          $(".precarga").append("<img src='img/back-homeapp-md.jpg' alt='img'><img src='img/back-seguro-md.jpg' alt='img'><img src='img/back-altainmediata-md.jpg' alt='img'><img src='img/back-bnamericas-md.jpg' alt='img'><img src='img/back-altoincendios-md.jpg' alt='img'><img src='img/back-levitar-md.jpg' alt='img'>");
-        }
-        else{
-          $(".precarga").append("<img src='img/back-homeapp.jpg' alt='img'><img src='img/back-seguro.jpg' alt='img'><img src='img/back-altainmediata.jpg' alt='img'><img src='img/back-bnamericas.jpg' alt='img'><img src='img/back-altoincendios.jpg' alt='img'><img src='img/back-levitar.jpg' alt='img'>")
-        }
-      });
-      //Loader
-      $(window).load(function() {
-        $(".loader").fadeOut("slow");
-        $("body").removeClass("fixed");
-        $(".precarga img").hide();
-      });
-      //Cover box
-      $(".item").hover(function(){
-        $(this).find(".cover").fadeIn("fast");
-        $(this).addClass("byn");
-      },function(){
-        $(this).find(".cover").fadeOut("fast");
-        $(this).removeClass("byn");
-      }
-      );
-    </script>
+    <script src="js/custom.js"></script>
     <!-- scripts -->
 
   </body>
--- js/custom.js
+++ js/custom.js
@@ -20,3 +20,44 @@ $(document).ready(function() {
 		}
     });
 });
+//wow
+var ancho = $(window).width();
+  if(ancho > 900){
+    wow = new WOW(
+      {
+        animateClass: 'animated',
+        offset:       100,
+        callback:     function(box) {
+          console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+        }
+      }
+    );
+    wow.init();
+  }
+//precarga imágenes
+$(document).ready(function(){
+  var ancho = $(window).width();
+  if(ancho < 1440){
+    $(".precarga").append("<img src='img/back-homeapp-md.jpg' alt='img'><img src='img/back-seguro-md.jpg' alt='img'><img src='img/back-altainmediata-md.jpg' alt='img'><img src='img/back-bnamericas-md.jpg' alt='img'><img src='img/back-altoincendios-md.jpg' alt='img'><img src='img/back-levitar-md.jpg' alt='img'>");
+  }
+  else{
+    $(".precarga").append("<img src='img/back-homeapp.jpg' alt='img'><img src='img/back-seguro.jpg' alt='img'><img src='img/back-altainmediata.jpg' alt='img'><img src='img/back-bnamericas.jpg' alt='img'><img src='img/back-altoincendios.jpg' alt='img'><img src='img/back-levitar.jpg' alt='img'>")
+  }
+});
+//Loader
+$(window).load(function() {
+  $(".loader").fadeOut("slow");
+  $("body").removeClass("fixed");
+  $(".precarga img").hide();
+});
+//Cover box
+var ancho = $(window).width();
+if(ancho > 900){
+  $(".item").hover(function(){
+    $(this).find(".cover").fadeIn("fast");
+    $(this).addClass("byn");
+  },function(){
+    $(this).find(".cover").fadeOut("fast");
+    $(this).removeClass("byn");
+  });
+}
